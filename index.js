8fd1917549193d245aece42814dcd01a6f88c829
Eduardo Silva <eduardofranciscone7@gmail.com>
2026-02-07 21:10:26 -0300

    adicionando estilo a lista

--- index.js
+++ index.js
@@ -5,32 +5,46 @@ allRides.forEach(async ([id, value]) => {
     const ride = JSON.parse(value);
     ride.id = id
 
+    const mapElement = document.createElement('div');
+    mapElement.style = 'width: 100px; height: 100px';
+    mapElement.classList.add('bg-secondary');
+    mapElement.classList.add('rounded-4');
+
+    const dataElement = document.createElement('div');
+    dataElement.className = 'flex-fill d-flex flex-column gap-2';
+
     const firstPosition = ride.data[0];
     const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude)
 
     const itemElement = document.createElement('li');
     itemElement.id = ride.id;
+    itemElement.className = 'd-flex align-items-center p-1 shadow-sm gap-3';
     
     const cityDiv= document.createElement('div');
     cityDiv.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`;
+    cityDiv.className = 'text-primary fw-bold mb-2';
 
     const maxSpeedDiv= document.createElement('div');
     maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)}`;
+    maxSpeedDiv.className = 'h5';
 
     const distanceDiv= document.createElement('div');
     distanceDiv.innerText = `Distance: ${getDistance(ride.data)}`;
 
     const durationDiv= document.createElement('div');
-    durationDiv.innerText = getDuration(ride);
-
+    durationDiv.innerText = `Duration: ${getDuration(ride)}`;
+    
     const dateDiv= document.createElement('div');
     dateDiv.innerText = getStartDate(ride);
+    dateDiv.className = 'text-secondary mt-2';
 
-    itemElement.appendChild(cityDiv);
-    itemElement.appendChild(maxSpeedDiv);
-    itemElement.appendChild(distanceDiv);
-    itemElement.appendChild(durationDiv);
-    itemElement.appendChild(dateDiv);
+    dataElement.appendChild(cityDiv);
+    dataElement.appendChild(maxSpeedDiv);
+    dataElement.appendChild(distanceDiv);
+    dataElement.appendChild(durationDiv);
+    dataElement.appendChild(dateDiv);
+    itemElement.appendChild(mapElement)
+    itemElement.appendChild(dataElement);
     rideListElement.appendChild(itemElement);
     
 }) 
